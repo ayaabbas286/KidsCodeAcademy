8a05fdf30a4a5d46647344deca7e330ccec1bc67
ayaabbas286 <ayaabbas286@gmail.com>
2026-09-23 12:42:07 +0300

    All programs

---
+++ src/app/pages/all-programs/all-programs.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+export interface ProgramCard {
+  id: number;
+  title: string;          
+  ageRange: string;       
+  ageIcon: string;       
+  description: string;    
+  features: string[];     
+  duration: string;     
+  projectsCount: string; 
+  characterImage: string;
+  badgeLogo: string;     
+  colorTheme: 'blue' | 'yellow' | 'purple' | 'green';
+  route: string;
+}
+@Component({
+  selector: 'app-all-programs',
+  imports: [],
+  templateUrl: './all-programs.html',
+  styleUrl: './all-programs.css',
+})
+export class AllPrograms {}
